7f38cfd5106c1165fac6b4968b64d70375c89e09
Mathew Oreoluwa Solomon <100151674+Solomonmatt@users.noreply.github.com>
2025-01-15 19:12:08 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -505,7 +505,8 @@ export default function Home() {
           <div className="hover-sw-nav hover-sw-2">
             <div
               dir="ltr"
-              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 wrap">
+              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 wrap"
+            >
               {products &&
                 products?.map((product) => {
                   return (
@@ -540,7 +541,7 @@ export default function Home() {
                             className="box-icon wishlist btn-icon-action"
                           >
                             <span className="icon icon-heart" />
-                           
+                            <span className="tooltip">Add to Wishlist</span>
                             <span className="icon icon-delete" />
                           </a>
                           <a
@@ -569,9 +570,30 @@ export default function Home() {
                           £{product?.price}
                         </span>
                         {
-                       
+                        /* 
+                        <ul className="list-color-product">
+                              <li className="list-color-item color-swatch active">
+                                <span className="tooltip">Golden Yellow</span>
+                                <span className="swatch-value bg_golden-yellow" />
+                                <img
+                                  className="lazyload"
+                                  data-src="images/products/shop1.png"
+                                  src="images/products/shop1.png"
+                                  alt="image-product"
+                                />
+                              </li>
+                              <li className="list-color-item color-swatch">
+                                <span className="tooltip">Silver White</span>
+                                <span className="swatch-value bg_white" />
+                                <img
+                                  className="lazyload"
+                                  data-src="images/products/shop1.png"
+                                  src="images/products/shop1.png"
+                                  alt="image-product"
+                                />
+                              </li>
                             </ul> 
-                            }
+                            */}
                       </div>
                     </div>
                   );
@@ -589,6 +611,7 @@ export default function Home() {
       {/* /Product */}
 
 
+      
       
       {/* Collection */}
       <section className="flat-spacing-12 pb_0">
