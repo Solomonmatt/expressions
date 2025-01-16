119990b10f753ae4af5c4132f2d7a26d8f402478
Mathew Oreoluwa Solomon <100151674+Solomonmatt@users.noreply.github.com>
2025-01-16 07:29:57 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2412,6 +2412,14 @@ export default function Home() {
       {/* /mobile menu */}
 
 
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+  $('.dropdown-toggle').dropdown();
+  $(document).on('click', '.dropdown-menu', function (e) {
+    e.stopPropagation(); // Prevents menu from closing when clicking inside it
+  
 
       {/* canvasSearch */}
       <div className="offcanvas offcanvas-end canvas-search" id="canvasSearch">
