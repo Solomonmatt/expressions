458545b934778425decf79cdb5e379e78a7846e0
Mathew Oreoluwa Solomon <100151674+Solomonmatt@users.noreply.github.com>
2026-06-26 21:18:19 +0100

    Update transaction.action.ts

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { redirect } from 'next/navigation'
 import Stripe from "stripe";
 
 export async function checkoutCart(transaction: any) {
@@ -28,24 +27,8 @@ export async function checkoutCart(transaction: any) {
     mode: 'payment',
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/account/orders`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-  })
+  });
 
-  redirect(session.url!)
+  // Instead of redirect(), return the URL
+  return { url: session.url };
 }
-
-// export async function createTransaction(transaction: CreateTransactionParams) {
-//   try {
-//     await connectToDatabase();
-
-//     // Create a new transaction with a buyerId
-//     const newTransaction = await Transaction.create({
-//       ...transaction, buyer: transaction.buyerId
-//     })
-
-//     await updateCredits(transaction.buyerId, transaction.credits);
-
-//     return JSON.parse(JSON.stringify(newTransaction));
-//   } catch (error) {
-//     handleError(error)
-//   }
-// }
